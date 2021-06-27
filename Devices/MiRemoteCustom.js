24b39d19b2388d3af4f91c68d59ced4c34f42178
Chua Chin Siang <ccs6327@gmail.com>
2021-06-27 16:16:02 +0800

    change dThis to this

--- Devices/MiRemoteCustom.js
+++ Devices/MiRemoteCustom.js
@@ -18,7 +18,7 @@ class MiRemoteCustomService {
     this.interval = interval;
 
     this.readydevice = false;
-    this.device = dThis.platform.getMiioDevice(
+    this.device = this.platform.getMiioDevice(
       {
         address: ip,
         token
